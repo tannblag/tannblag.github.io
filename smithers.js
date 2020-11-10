b24015ce8f925050e2076142b1ff737158d15f50
Richard Anderson <richande218@gmail.com>
2020-11-09 19:10:02 -0600

    PlaySound function added

--- smithers.js
+++ smithers.js
@@ -6,4 +6,8 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
-  
+  function PlaySound(melody) {
+    var path = "media\\"
+    var snd = new Audio(path + melody + ".mp3");
+    snd.play();
+  }
