ede946ee6634bff96169406549cc2382c5a5fe52
tannblag <tannblag@gmail.com>
2020-11-09 19:15:23 -0600

    comments

--- smithers.js
+++ smithers.js
@@ -1,3 +1,4 @@
+// Side Bar //
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
@@ -5,9 +6,12 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+// End Side Bar//
 
+//Plays sound//
   function PlaySound(melody) {
     var path = "media\\"
     var snd = new Audio(path + melody + ".mp3");
     snd.play();
   }
+//End Plays Sound//
